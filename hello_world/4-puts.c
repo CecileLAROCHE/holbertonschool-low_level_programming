cb8a54d98f08dc5ee2b4d8a3bd4cd8445269c37b
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-16 17:23:43 +0200

    fichier 5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,14 @@
 /**
 * main - Entry point
 *
+* ecrit ("Programming is like building a multilingual puzzle) et reviens
+* à la ligne automatiquement.
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/*puts = ecrit en rajoutant une ligne vide a la fin*/
 return (0);
 }
