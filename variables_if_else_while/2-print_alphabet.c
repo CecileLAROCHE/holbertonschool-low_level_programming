085f519410379e52b36a0706480ed47dcee5345e
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-17 16:48:07 +0200

    exe 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
 * main - Entry point
@@ -10,4 +8,10 @@
 * Return: 0
 */
 int main(void)
-{ int alphabet = a;
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
