d191d2fa895d572b65c91aecc152520584e8e785
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-17 16:13:03 +0200

    invertion 6 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,13 +22,12 @@ if (last > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
-else if (last < 6)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-}
-else
+else if (last ==0)
 {
 printf("Last digit of %d is %d and and is 0\n", n, last);
+}
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 return (0);
 }
