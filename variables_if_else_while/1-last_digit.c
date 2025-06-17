adb58ffaca25d105a10401c3ed4ec96c9b2030e3
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-17 15:04:55 +0200

    tast 1 end i hoppe

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,9 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Description: Longer description of the function
+*
+* Return: 0
+*/
 int main(void)
 {
 	int n;
@@ -13,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
 printf("Last digit of %d is %d and is greater than 5", n, 10 % n);
-if else (n % 10 < 6)
+else if (n % 10 < 6)
 printf("Last digit of %d is %d and and is 0", n, 10 % n);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0", n, 10 % n);
