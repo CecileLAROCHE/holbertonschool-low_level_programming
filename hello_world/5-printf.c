642fd7d4137654a6443f55e247aff3524e5a2ee7
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-17 09:35:06 +0200

    test 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,12 @@
 /**
 * main - Entry point
 *
+* Description: 'the program's description'
+*
 * return 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
