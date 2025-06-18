9114ee4b6b3d6003ed38366f9670e30c6af68481
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-18 11:23:49 +0200

    2nd corection ex 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,17 +12,17 @@ int main(void)
 char num;
 
 for (num = '0' ; num <= '9' ; num++)
+putchar(num);
 
 if (num != '9')
 {
-putchar(num);
 putchar(',');
 putchar(' ');
 }
 else
 {
-putchar(num);
 putchar('\n');
 }
+
 return (0);
 }
