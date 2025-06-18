25feebbd63b7d559442867bed6b1bf32e1398ce9
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-18 10:52:36 +0200

    ex 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Longer description of the function
+*
+* Return: 0
+*/
+int main(void)
+{
+int x;
+for (x = 48 ; x < 58 ; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
