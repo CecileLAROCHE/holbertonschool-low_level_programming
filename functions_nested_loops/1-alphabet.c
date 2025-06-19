9c3df5b1805459791b83a80390540d29ba99e63b
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-19 10:07:24 +0200

    ex 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
-1-alphabet.c
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: Longer description of the function
+*
+* Return: 0
+*/
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+
+	return (0);
+}
