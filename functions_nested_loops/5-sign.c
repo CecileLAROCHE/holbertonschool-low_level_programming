37a76ff705e6d5a798ab922893ed4c3a288ab311
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-19 16:47:50 +0200

    ex 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - Entry point
+* @n: The character to be checked.
+*
+*Écrivez une fonction qui affiche le signe d'un nombre.
+*
+*Prototype : int print_sign(int n);
+*Renvoie 1 et affiche + si n est supérieur à zéro
+*Renvoie 0 et affiche 0 si n est nul
+*Renvoie -1 et affiche - si n est inférieur à zéro
+*
+* Return: 0
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+else if (n == 0)
+_putchar('0');
+	else
+	_putchar('-');
+
+return (0);
+}
