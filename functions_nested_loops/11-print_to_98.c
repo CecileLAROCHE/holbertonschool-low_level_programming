693a4a040f7e51baf6918558e20751aa5b1b2c96
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-20 17:14:26 +0200

    ex 11 fin?

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,22 +14,17 @@
 void print_to_98(int n)
 
 {
-if (n == 98)
-	{
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
-	}
-else if (n < 98)
-	{
-		while (n < 98)
-		printf("%d", n++);
-	printf("%d", n);
-	}
-	else
+	if (n >= 98)
 	{
 		while (n > 98)
-		printf("%d", n--);
-	printf("%d", n);
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-_putchar('\n');
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
