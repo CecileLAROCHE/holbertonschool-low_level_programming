d03e218f1ee5cc8ef9c78764c9d68625a58fda33
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-20 17:04:52 +0200

    ex 11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - Entry point
+* @a: premier nb
+* @b: second nb
+*
+*Write a function that adds two integers and returns the result.
+*
+* Return: Always 0
+*/
+
+int add(int a, int b)
+
+{
+return (a + b);
+}
