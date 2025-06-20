d6de91e8ecf5491fa2d8231f0b8847334c04f028
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-20 09:57:39 +0200

    ex 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - Entry point
+* @i: The character to be checked.
+*
+*Écrivez une fonction qui calcule la valeur absolue d'un entier.
+*
+*Prototype : int _abs(int);
+*
+* Return: 0
+*/
+
+int _abs(int i)
+{
+if (i > 0)
+	{
+	return (i);
+	}
+else
+{
+	return (-i);
+}
+}
