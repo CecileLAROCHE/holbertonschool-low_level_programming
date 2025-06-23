09b91310887c7d5334762ff0366c7a594192c085
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-23 12:07:46 +0200

    ex 7 second essai

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -18,16 +18,18 @@ void print_diagonal(int n)
 {
 int i;
 
-if (n > 0)
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
 {
 	for (i = 0; i < (n - 1); i++)
 	{
 		_putchar(' ');
 	}
-}
-else
-{
 	_putchar('/');
 }
 _putchar('\n');
 }
+
