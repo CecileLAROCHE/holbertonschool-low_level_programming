f75f16a642d722df7b199425295cbeeb99451634
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-23 10:51:30 +0200

    ex 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
-* print_numbers - entry point
+*print_most_numbers: entry point
+*
 *
 *Écrivez une fonction qui affiche les nombres de 0 à 9,
 *suivis d'un saut de ligne.
 *
-*Prototype : void print_numbers(void);
+*Prototype : void print_most_numbers(void);
+*N'affichez pas 2 et 4.
 *Vous ne pouvez utiliser _putchar que deux fois dans votre code.
 *
-* Return: void
+*Return: void
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 char num;
 for (num = '0' ; num <= '9' ; num++)
-_putchar(num);
+	{
+	if ((num != '2') && (num != '4'))
+		{
+		_putchar(num);
+		}
+	}
 _putchar('\n');
 }
