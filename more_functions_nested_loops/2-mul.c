f5b91a8b6a9792756efdbc1d742c091a322702e7
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-23 10:17:56 +0200

    ex 2 - betty not happy

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - entry point
+*@a - The first integer to be multiplied.
+*@b - The second integer to be multiplied.
+*
+* Write a function that multiplies two integers.
+*
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
