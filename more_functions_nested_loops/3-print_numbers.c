b1af2a4b57abf5f33c8843958565609f51702447
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-23 10:27:57 +0200

    ajout _putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - entry point
+*
+*Écrivez une fonction qui affiche les nombres de 0 à 9, suivis d'un saut de ligne.
+*
+*Prototype : void print_numbers(void);
+*Vous ne pouvez utiliser _putchar que deux fois dans votre code.
+*
+* Return: void
+*/
+
+void print_numbers(void)
+{
+
+}
