68c91caefa92e3e37f1b8aae4da5e9c92d74ed55
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-23 10:32:28 +0200

    ex 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,19 @@
 /**
 * print_numbers - entry point
 *
-*Écrivez une fonction qui affiche les nombres de 0 à 9, suivis d'un saut de ligne.
+*Écrivez une fonction qui affiche les nombres de 0 à 9,
+*suivis d'un saut de ligne.
 *
 *Prototype : void print_numbers(void);
 *Vous ne pouvez utiliser _putchar que deux fois dans votre code.
 *
-* Return: void
+* Return: 0
 */
 
 void print_numbers(void)
 {
-
+char num;
+for (num = '0' ; num <= '9' ; num++)
+_putchar(num);
+_putchar('\n');
 }
