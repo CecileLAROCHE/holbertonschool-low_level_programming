240a4924d633b54be3907ad73e1caed150d3e7fa
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-24 10:56:46 +0200

    ex 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*reset_to_98 - main entry
+*@n:pointer
+*
+*Écrivez une fonction qui prend un pointeur vers un int en paramètre
+*et met à jour la valeur vers laquelle il pointe à 98.
+*
+*
+*
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
