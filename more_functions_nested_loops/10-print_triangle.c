44953019c3f9eb0a1b1d7cb2fba045fa93430261
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-24 09:45:46 +0200

    betty happy

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,17 +15,17 @@ int hash;
 int index;
 
 if (size > 0)
-	{
-		for (hash = 1; hash <= size; hash++)
-		{
-			for (index = size - hash; index > 0; index--)
-				_putchar(' ');
-			for (index = 0; index < hash; index++)
-				_putchar('#');
-			if (hash == size)
-				continue;
-			_putchar('\n');
-		}
-	}
+{
+for (hash = 1; hash <= size; hash++)
+{
+for (index = size - hash; index > 0; index--)
+_putchar(' ');
+for (index = 0; index < hash; index++)
+_putchar('#');
+if (hash == size)
+continue;
+_putchar('\n');
+}
+}
 _putchar('\n');
 }
