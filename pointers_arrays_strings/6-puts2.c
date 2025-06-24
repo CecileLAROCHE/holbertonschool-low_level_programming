8b79d8d3dfcf08217d24e70a2c7608f7ecad6f39
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-24 15:36:44 +0200

    ex 4

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*puts2 - main entry
+*@str: string
+*
+*Écrivez une fonction qui affiche un caractère sur deux d'une chaîne,
+*en commençant par le premier caractère, suivi d'une nouvelle ligne.
+*/
+
+void puts2(char *str)
+{
+
+
+
+}
