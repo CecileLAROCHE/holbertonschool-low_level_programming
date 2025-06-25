7261dfabe2903207725c34cbb19702c68ab76070
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-25 10:50:13 +0200

    debut ex 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,23 @@
 *Écrivez une fonction qui inverse une chaîne.
 */
 
-void rev_string(char *s);
+void rev_string(char *s)
 
 {
+int length = 0; /*longueur de la chaine de caratère*/
+int tmp; /*variable temporaire*/
+int index; /*position sur la chaine de caractère*/
 
+/*calcul de la longeur de la chaine de caractère*/
+while (*s++)
+	length++;
+
+/*boucle pour evolution de l'index de 1 à (lenght/2)*/
+for (index = 0; index < length / 2 ; index++)
+	{
+	/*Swapping values of a and  b*/
+	tmp = s[index];
+	s[index] = s[length - index];
+	s[length - index] = tmp;
+	}
 }
