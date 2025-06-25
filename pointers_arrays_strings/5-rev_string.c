6771dba53863b69d8407b223bfb4e538b178974d
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-25 11:33:51 +0200

    ex 5 nl essai

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -17,12 +17,13 @@ int index = 0; /*position sur la chaine de caractère*/
 
 /*boucle pour evolution de l'index de 1 à (lenght/2)*/
 while (s[index++])
-		length++;
-for (index = 0; index <= length / 2  ; index++)
+	length++;
+
+for (index = 0; index <= - 1 + length / 2  ; index++)
 	{
 	/*echange valeur debut et fin*/
-		tmp = s[index];
-		s[index] = s[length - index - 1];
-		s[length - index - 1] = tmp;
+	tmp = s[index];
+	s[index] = s[length - index - 1];
+	s[length - index - 1] = tmp;
 	}
 }
