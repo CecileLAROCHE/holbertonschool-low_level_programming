2cbac60173599dc013e7341fae2b2a48fc4418c5
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-25 11:31:27 +0200

    ex 5 betty pas contente

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *rev_string - main entry
@@ -12,18 +13,16 @@ void rev_string(char *s)
 {
 int length = 0; /*longueur de la chaine de caratère*/
 char tmp; /*variable temporaire*/
-int index; /*position sur la chaine de caractère*/
-
-/*calcul de la longeur de la chaine de caractère*/
-while (*s++)
-	length++;
+int index = 0; /*position sur la chaine de caractère*/
 
 /*boucle pour evolution de l'index de 1 à (lenght/2)*/
-for (index = 0; index < length / 2 ; index++)
+while (s[index++])
+		length++;
+for (index = 0; index <= length / 2  ; index++)
 	{
-	/*Swapping values of a and  b*/
-	tmp = *(s + index);
-	*(s + index) = *(s + length - index);
-	*(s + length - index) = tmp;
+	/*echange valeur debut et fin*/
+		tmp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = tmp;
 	}
 }
