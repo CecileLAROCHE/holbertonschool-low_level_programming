d95ad1a3b8388a94777037e629349becd68857d3
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-26 17:02:42 +0200

    ex 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strcmp - fonction qui copie une chaîne.
+*@s1: destination
+*@s2: second char
+*
+*Écrivez une fonction qui compare deux chaînes.
+*
+*Return: dest
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+while (*s1 && *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+
+return (*s1 - *s2);
+}
