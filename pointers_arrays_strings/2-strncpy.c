1fc3e9794ca3cf2bf161f8d439400e1926bd00f1
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-26 16:44:56 +0200

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,8 +6,6 @@
 *@src: second char
 *@n: troisième
 *
-*Votre fonction devrait fonctionner exactement comme strncpy.
-*
 *Return: dest
 */
 
@@ -15,8 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 {
 int index = 0; /*position sur la chaine de caractère*/
+
+
 for (index = 0 ; index < n && src[index] != '\0' ; index++)
 dest[index] = src[index];
+
 for ( ; index < n ; index++)
 dest[index] = '\0';
 
