54c96881792bb4732290aaba805ffde621081e3d
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-27 12:52:37 +0200

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,6 +11,8 @@
 *Les lettres o et O doivent être remplacées par 0
 *Les lettres t et T doivent être remplacées par 7
 *Les lettres l et L doivent être remplacées par 1
+*
+*Return: leet
 */
 
 
@@ -26,10 +28,12 @@ int i;
 while (str[index] != '\0')
 {
 	for (i = 0; i < 5; i++)
-if (str[index] == Maj[i] || str[index] == Min[i])
 	{
-	str[index] = chif[i];
-	break;
+		if (str[index] == Maj[i] || str[index] == Min[i])
+		{
+		str[index] = chif[i];
+		break;
+		}
 	}
 	index++;
 }
