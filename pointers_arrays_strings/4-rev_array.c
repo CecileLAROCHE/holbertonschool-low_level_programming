6f75e9031a9138bd22b898503512b5ac4daa9c74
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-27 12:12:40 +0200

    ex 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,7 @@
 void reverse_array(int *a, int n)
 
 {
-
-char tmp; /*variable temporaire*/
+int tmp; /*variable temporaire*/
 int index = 0; /*position sur la chaine de caractère*/
 
 /*boucle pour evolution de l'index de 1 à (lenght/2)-1*/
@@ -24,9 +23,5 @@ for (index = n - 1; index >= n / 2  ; index--)
 	tmp = a[index];
 	a[index] = a[n - index - 1];
 	a[n - index - 1] = tmp;
-	
-
-
 }
-return(index)
 }
