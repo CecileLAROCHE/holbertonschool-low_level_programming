01afe102028f82d10e394006a87a243822913f57
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-30 10:46:19 +0200

    ex 0

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_memset - main entry
+*@dest: target
+*@src: constant byte
+*@n: number of byte
+*
+*ecrire un fonction qui copie l'espace memoire
+*
+*Return: returns new value of target
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+while (n)
+{
+	s[n - 1] = scr;
+	n--
+}
+return ()
+
+
+
+}
--- pointers_arrays_strings/proj_2/main.h
+++ pointers_arrays_strings/proj_2/main.h
@@ -27,9 +27,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
-/*project 3*/
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-
 #endif
