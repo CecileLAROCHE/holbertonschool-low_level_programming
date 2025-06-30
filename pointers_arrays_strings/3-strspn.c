e4889ef03c2488231a469b2bfbbe186cda14ef2c
CecileLAROCHE <11458@holbertonstudents.com>
2025-06-30 14:52:00 +0200

    ex 4 betty pas contente :(

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * yu 
 *_strspn - Get the length of a prefix substring
 *@s: the string to be scanned
 *@accept: the string containing the list of characters to match
--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,29 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
+*_strpbrk - searches a string for any of a set of bytes.
+*@s: The string to search.
+*@accept: The string containing the bytes to match.
+*
+*Return: A pointer to the byte in s that matches one of the bytes in accept,
+*or NULL if no such byte is found.
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int index;
+
+while (*s != '\0')
+{
+for (index = 0; accept[index] != '\0'; index++)
+{
+if (*s == accept[index])
+{
+return (s);
+}
+}
+s++;
+}
+return (NULL);
+}
