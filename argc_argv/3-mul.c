33ff6639544f1a78d6cb8a5857869db03842a103
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-02 15:58:39 +0200

    ex 33

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
+*main - Multiplies two numbers provided as command-line arguments.
+*@argc: Argument count.
+*@argv: Argument vector.
 *
-*
+*Return: 0 on success, 1 if the number of arguments is incorrect.
 */
 
 int main(int argc, char *argv[])
 {
-int i;
+int num1;
+int num2;
+int result;
 
-if (arg[i] || arg['\0'])
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
 
-
-
+printf("%d\n", result);
+return (0);
 }
