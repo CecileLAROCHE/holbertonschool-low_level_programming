f6c785b63db1e654c14f784c531b61b1cf327e5e
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-07 14:04:02 +0200

    ex 2 encore un essai

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -30,18 +30,23 @@ while (*s2++)
 	length2++;
 
 /*Allocate memory for the concatenated string*/
-concat = malloc(length1 + length2 + 1);
+concat = malloc(sizeof(char) * (length1 + length2 + 1));
+
+/*Check if memory allocation was successful*/	
 if (concat == NULL)
 return (NULL);
 
-for (index1 = 0; index1 < length1; index1++)
+for (index1 = 0; s1[index1] != '\0'; index1++)
 {
-    concat[index1] = s1[index1];
-}   
-
-for (index2 = 0; index2 < length2; index2++)
+	concat[index1] = s1[index1];
+}	
+for (index2 = 0; s2[index2] != '\0'; index2++)
 {
-    concat[index1 + index2] = s2[index2];
+	concat[index1 + index2] = s2[index2];
 }
+
+
+concat[index1 + index2] = '\0';
+
 return (concat);
 }
