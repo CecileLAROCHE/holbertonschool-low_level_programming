5c148fa5ac829fa767269b4842c70dc1a1158f6e
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-08 17:07:30 +0200

    dd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -39,26 +39,36 @@ unsigned i;
 
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
+{
 	return (NULL);
+}
 
 name_copy = malloc(_strlen(name) + 1);
 if (name_copy == NULL)
+{
 	free(dog);
 	return (NULL);
+}
 
 for (i = 0; name[i] != '\0'; i++)
+{
 name_copy[i] = name[i];
 name_copy[i] = '\0';
+}
 
 owner_copy = malloc(_strlen(owner) + 1);
 if (owner_copy == NULL)
-free(name_copy);
-free(dog);
-return (NULL);
+{
+	free(name_copy);
+	free(dog);
+	return (NULL);
+}
 
 for (i = 0; owner[i] != '\0'; i++)
-	owner_copy[i] = owner[i];
-	owner_copy[i] = '\0';
+{
+owner_copy[i] = owner[i];
+owner_copy[i] = '\0';
+}
 
 	dog->name = name_copy;
 	dog->age = age;
