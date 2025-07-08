fd49395197558407cefc376670599eaf44f6aa7d
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-08 16:03:32 +0200

    ex 4 + 5

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,16 +1,51 @@
-#include <dog.h>
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * new_dog - Creates a new dog structure
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * This function allocates memory for a new dog structure,
- * initializes its members with the provided values, and returns
- * a pointer to the newly created dog structure.
- *
- * Return: Pointer to the newly created dog structure, or NULL on failure.
- */
+*new_dog - Creates a new dog structure
+*@name: Name of the dog
+*@age: Age of the dog
+*@owner: Owner of the dog
+*
+*This function allocates memory for a new dog structure,
+*initializes its members with the provided values, and returns
+*a pointer to the newly created dog structure.
+*
+*Return: Pointer to the newly created dog structure, or NULL on failure.
+*/
 
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
+
+{
+    dog_t *dog;
+    char *name_copy;
+    char *owner_copy;
+
+dog = malloc(sizeof(dog_t));;
+if (dog == NULL)
+    return (NULL); 
+    
+name_copy = malloc(sizeof(name));   
+if (name_copy == NULL)
+    {
+        free(dog);
+        return (NULL);
+    }
+strcpy(name_copy, name);
+
+owner_copy = malloc(sizeof(owner));
+if (owner_copy == NULL)
+    {
+        free(name_copy);
+        free(dog);
+        return (NULL);
+    }
+strcpy(owner_copy, owner);
+
+dog->name = name_copy;
+dog->age = age;
+dog->owner = owner_copy;
+
+    return (dog);
+}
--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,21 @@
-#include <dog.h>
+#include "dog.h"
 
 /**
- * free_dog - Frees a dog structure
- * @d: Pointer to the dog structure to free
- *
- * This function frees the memory allocated for the name and owner
- * strings of the dog structure, and then frees the structure itself.
- * It is important to ensure that the pointer is not NULL before freeing.
- */
+*free_dog - Frees a dog structure
+*@d: Pointer to the dog structure to free
+*
+*This function frees the memory allocated for the name and owner
+*strings of the dog structure, and then frees the structure itself.
+*It is important to ensure that the pointer is not NULL before freeing.
+*/
 
 void free_dog(dog_t *d)
+
+{
+    if (d != NULL)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
