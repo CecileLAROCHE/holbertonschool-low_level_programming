42c8791ac106f93612e869bdffd020ef286dda29
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-08 14:56:01 +0200

    ex 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
 *print_dog - Prints the details of a dog structure
@@ -15,7 +16,34 @@ if (d == NULL)
 	return;
 }
 
+if (d->name == NULL)
+{
+	printf("Name: (nil)\n");
+}
+else
+{
+	printf("Name: %s\n", d->name);
+}
 
+if (d->age < 0) 
+{
+	printf("Age: (nil)\n");
+}
+else if (d->age == 0.0	)
+{
+	printf("Age: (nil)\n");
+}
+else
+{
+	printf("Age: %.1f\n", d->age);
+}
 
-
+if (d->owner == NULL)
+{
+	printf("Owner: (nil)\n");
+}
+else
+{
+	printf("Owner: %s\n", d->owner);
+}
 }
