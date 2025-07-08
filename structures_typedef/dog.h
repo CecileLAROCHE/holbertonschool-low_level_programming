34a135de3ee6c8efea4410586de0cec04046bba0
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-08 13:44:05 +0200

    betty ?

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,14 +7,13 @@
 *@name: Name of the dog
 *@age: Age of the dog
 *@owner: Owner of the dog
-*This structure represents a dog with its name, age, and owner.
 */
 
 struct dog
 {
-char* name;
-float age;
-char* owner;
+	char* name;
+	float age;
+	char* owner;
 };
 
 #endif
