e14da04ae645d7f99a8a0aa645aeadcdba0de274
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-09 10:04:33 +0200

    correction ex 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,21 +7,23 @@
 *This function prints the name, age, and owner of the dog.
 *If any of the members are NULL, it prints "(nil)" for that member.
 *Return: void
-*/ 
+*/
 
 void print_dog(struct dog *d)
 {
+if (d == NULL)
+	return;
 
 if (d->name == NULL)
 	printf("Name: (nil)\n");
 else
 	printf("Name: %s\n", d->name);
 
-if (d->age < 0) 
+if (d->age < 0)
 {
 	printf("Age: (nil)\n");
 }
-else if (d->age == 0.0	)
+else if (d->age < 0.0)
 {
 	printf("Age: (nil)\n");
 }
