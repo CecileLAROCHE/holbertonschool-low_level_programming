efa19125500a324516e45c68d2ec732775cb9826
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-09 10:26:53 +0200

    betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,7 +21,6 @@ return (len);
 *@name: Name of the dog
 *@age: Age of the dog
 *@owner: Owner of the dog
-
 *This function allocates memory for a new dog structure,
 *initializes its members with the provided values, and returns
 *a pointer to the newly created dog structure.
