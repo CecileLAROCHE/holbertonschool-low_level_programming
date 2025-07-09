96cc1595bbc743d69e30cf2ce5ba5c016be192ec
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-09 10:24:06 +0200

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ return (len);
 *@name: Name of the dog
 *@age: Age of the dog
 *@owner: Owner of the dog
-*
+
 *This function allocates memory for a new dog structure,
 *initializes its members with the provided values, and returns
 *a pointer to the newly created dog structure.
@@ -33,9 +33,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 {
 dog_t *dog;
-char *name_copy;
-char *owner_copy;
-unsigned i;
+char *name_copy, *owner_copy;
+unsigned int i;
 
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
@@ -53,8 +52,9 @@ if (name_copy == NULL)
 for (i = 0; name[i] != '\0'; i++)
 {
 name_copy[i] = name[i];
-name_copy[i] = '\0';
 }
+name_copy[i] = '\0';
+
 
 owner_copy = malloc(_strlen(owner) + 1);
 if (owner_copy == NULL)
@@ -67,12 +67,12 @@ if (owner_copy == NULL)
 for (i = 0; owner[i] != '\0'; i++)
 {
 owner_copy[i] = owner[i];
-owner_copy[i] = '\0';
 }
+owner_copy[i] = '\0';
+
 
 	dog->name = name_copy;
 	dog->age = age;
 	dog->owner = owner_copy;
-
 	return (dog);
 }
