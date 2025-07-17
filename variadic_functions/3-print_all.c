176b713dcc76e84168418db73fb76d8234b174bd
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-17 14:55:10 +0200

    ecriture du code projet 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
 * print_all - Prints anything
@@ -10,5 +12,34 @@
 
 void print_all(const char * const format, ...)
 {
+	unsigned int i = 0;
+	va_list args;
+	char *str;
+	char *separator = "";
 
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		    format[i] == 'f' || format[i] == 's')
+		{
+		printf("%s", separator);
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+			}
+		separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
 }
