1cb6d7360dc5a42c6af532f0a6b5811d3aa5c509
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-28 10:54:22 +0200

    Creation du répertoire et des ficiers du projet C - Singly linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,9 @@
+
+
+/**
+ * print_list - prints all the elements of a singly linked list
+ * @h: pointer to the head of the list
+ * * Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
