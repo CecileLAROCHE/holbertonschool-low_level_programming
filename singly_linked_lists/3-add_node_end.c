d9256818294b0996e7914e1c48ae4af2b1fa36ec
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-28 15:55:53 +0200

    code ex 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,5 +7,40 @@
  * Return: address of the new element, or NULL if it failed
  */
 
-list_t *add_node_end(list_t **head, const char *str);
-{}
+list_t *add_node_end(list_t **head, const char *str)
+
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	unsigned int i = 0;
+	list_t *temp = *head;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (new_node->str[i] != '\0')
+		i++;
+
+	new_node->len = i;
+
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct list_t
 	struct list_t *next;
 } list_t;
 
-
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -29,3 +29,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
