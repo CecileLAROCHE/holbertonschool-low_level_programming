518ef2da5f0490ba587d61e310cf114f8ca74472
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-29 12:11:28 +0200

    Code de la tache 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
  * @h: pointer to the head of the list
@@ -7,6 +8,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t count = 0;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+return (count);
 }
 
