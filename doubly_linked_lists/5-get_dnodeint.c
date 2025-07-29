6cf7472c98b1e83f0ef197c452352f437f5a6c7d
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-29 15:53:47 +0200

    code task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,4 +8,17 @@
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{ }
+{
+	unsigned int i = 0;
+
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		head = head->next;
+	}
+	return (NULL);
+}
+
