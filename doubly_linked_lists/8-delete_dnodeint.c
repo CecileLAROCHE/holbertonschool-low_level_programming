3301dd5cc79f7aa8cabfd5d2850d22215aabb4a7
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-07-30 15:42:38 +0200

    code ex 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,5 +10,43 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i = 0;
+	dlistint_t *current = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return (-1);
+	}
+
+	if (current->prev == NULL)
+	{
+		*head = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = NULL;
+		}
+	}
+		else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = current->prev;
+		}
+	}
+
+	free(current);
+	return (1);
 
 }
+
