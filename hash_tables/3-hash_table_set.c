3a0e3c26b1ac3ae074af9bd273f44b2da15314da
CecileLAROCHE <11458@holbertonstudents.com>
2025-07-31 17:55:36 +0200

    push task 3 fin de journée

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,5 +13,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == "" || value == "")
 		return (0);
 
-	if ()
+	if (ht == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (ht->array[index])
+	
+
 }
