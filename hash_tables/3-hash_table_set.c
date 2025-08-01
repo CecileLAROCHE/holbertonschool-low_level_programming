d94d6a232abaf700f7e7ca7865842cd08b93ff50
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-08-01 10:06:17 +0200

    correction pour avoir le bon nombre de ligne task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,8 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
-	hash_node_t *new_node;
+	hash_node_t *node, *new_node;
 
 	if (key == NULL || value == NULL || ht == NULL || *key == '\0')
 		return (0);
@@ -23,8 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
-		{
-			free(node->value);
+		{ free(node->value);
 			node->value = strdup(value);
 			if (node->value == NULL)
 				return (0);
@@ -42,8 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 
 	if (new_node->key == NULL || new_node->value == NULL)
-	{
-	free(new_node->key);
+	{ free(new_node->key);
 	free(new_node->value);
 	free(new_node);
 	return (0);
