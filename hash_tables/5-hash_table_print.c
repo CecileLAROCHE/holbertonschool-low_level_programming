42cea7d9c312de3858ce34d3d06f92289db80a1d
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-08-01 16:28:40 +0200

    task 45 modification des "" de la sortie

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -29,7 +29,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (first == 0)
 				printf(", ");
-			printf("\"%s\": \"%s\"", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			first = 0;
 			node = node->next;
 		}
