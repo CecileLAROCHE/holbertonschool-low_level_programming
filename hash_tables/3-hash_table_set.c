753df799900c7d5e8e83b1bf47dae3410943aa93
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-08-01 09:47:13 +0200

    code task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,15 +10,51 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (key == "" || value == "")
-		return (0);
+	unsigned long int index;
+	hash_node_t *node;
+	hash_node_t *new_node;
 
-	if (ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-	for (ht->array[index])
-	
+	while (node)
+	{ /*verification de l'existance de la clé*/
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			if (node->value == NULL)
+			{
+				return (0);
+			}
+		return (1);
+		}
+	node = node->next;
+	}
 
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == 0)
+		{
+			return (0);
+		}
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+	free(new_node->key);
+	free(new_node->value);
+	free(new_node);
+	return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
+
